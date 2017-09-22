fc01c05ed586eeccbe75e6fc911980f269b96f1b
Brandon Klimek <sixthpoint@gmail.com>
2017-09-22 12:22:49 -0500

    api defaults

--- src/main/java/com/sixthpoint/springboot/codepipeline/example/controller/ApiController.java
+++ src/main/java/com/sixthpoint/springboot/codepipeline/example/controller/ApiController.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping( value = "/rest" )
+@RequestMapping( value = "/api" )
 public class ApiController {
 
     @RequestMapping( value = "/", method = RequestMethod.GET )
-    public List<String> displayAll() {
+    public List<String> index() {
         List<String> s = new ArrayList<>();
         s.add("Docker + AWS Codepipline Tutorial");
+        s.add("Learn more at: https://github.com/sixthpoint/Docker-AWS-CodePipeline");
         return s;
     }
-
 }
